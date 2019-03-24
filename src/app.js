5812ad6582bb361afb1723d9c0d0cf6515a68b65
aziemianski <artzie92@gmail.com>
2019-03-24 21:48:08 +0100

    Initial template

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+
+//ReacDOM.render(<CssTestComponent />, document.getElementById("page"));
+ReacDOM.render(<div>Hello React!</div>, document.getElementById("page"));
